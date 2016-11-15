0242ad9b145e481331e8d8c8481abca057496a6f
Adam Wilson <adamwilson1234@gmail.com>
2016-11-15 16:14:33 -0600

    more practice with objects using derek banas obj oriented js tutorial on youtube. various setter and getter syntax examples. calling them is done like assigning and getting value to/from a property (not like a method).

--- 06_objects/06_exercises.js
+++ 06_objects/06_exercises.js
@@ -47,7 +47,7 @@ Object.defineProperty(woofer, 'wooferEnumerate', {enumerable: true, value: 'hell
 
 // constructor given, hello everybody, hello all,
 for (var key in woofer){
-    console.log(key); // will include
+    // console.log(key); // will include
 }
 // make an object without any prototype. a clean, lonely new thing.
 var noRoots = Object.create(null);
@@ -55,3 +55,112 @@ noRoots.justMine = 'this is only mine';
 for (var item in noRoots){
     console.log(item);
 }
+
+// DEFINING A GETTER SYNTAX ALLOWING RETRIEVAL BY NAME
+var d = Date.prototype;
+console.log(d);
+Object.defineProperty(d, 'year', {
+    get: function(){
+        return this.getFullYear();
+    }
+});
+var now = new Date();
+console.log(now.year);
+
+/*===========================================
+DEREK BANAS OBJ ORIENTED JS
+=============================================*/
+var customer = {
+    name: "tom",
+    telephone: '708-805-5853',
+    speak: function(){
+        return "my name is " + this.name;
+    },
+    address: {
+        street: '1212 michigan',
+        city: 'Chicago',
+        state: 'IL'
+    },
+    set telSet(telNum){
+        this.telephone = telNum;
+        console.log(this.telephone);
+    },
+    get telGet(){
+        return this.telephone;
+    }
+};
+// call setter with a new number
+customer.telSet = '708-524-0872'; // set like a property don't call like a function
+console.log(customer.telGet); // call like a prop, not like a function
+
+
+
+
+customer.address.country ='U.S.';
+document.write(customer.speak() + '<br/>');
+
+function Person(name, street){
+    var secretNum = 75; // private variable
+    this.guessNum = function(guess){ // clues to guesser
+        if (guess > 75){
+            return 'Lower';
+        } else if (guess < 75){
+            return 'Higher';
+        } else {
+            return 'Correct!';
+        }
+    }
+    this.name = name;
+    this.street = street;
+    this.info = function(){
+        return this.name + ' lives on ' + this.street;
+    };
+}
+var bobSmith = new Person('Bob Smith', 'Wabash');
+console.log(bobSmith.guessNum(75));
+document.write(bobSmith.info() + '<br/>' + (bobSmith instanceof Person) + '<br/>');
+
+// DEPRECATED BUT COMMON WAY TO DEFINE SETTER, GETTER ON prototype
+// args = target, new prop, value(s)
+Object.__defineSetter__.call(Person.prototype, 'setStreet', function(streetInput){
+    this.street = streetInput;
+});
+// test the setter
+bobSmith.setStreet = '123 main street';
+console.log(bobSmith);
+
+// DEFINE A NICE CLEAN MINIMAL CONSTRUCTOR FOR A CIRCLE W JUST 1 PROP: RADIUS
+// ADD SET AND GET RADIUS METHODS TO IT'S PROTOYPE AND A GET AREA THAT USES PI*R2D
+// INSTANTIATE A CIRCLE Object, SET ITS RADIUS, DOC.WRITE ITS AREA
+function Circle(radius){
+    this._radius = radius;
+}
+Circle.prototype = {
+    set radius(radius) { this._radius = radius },
+    get radius() { return this._radius },
+    get area() { return Math.PI * (this._radius * this._radius) }
+};
+var myCircle = new Circle(5);
+myCircle.radius = 10;
+console.log(myCircle.radius + ' ' + myCircle.area.toFixed(2));
+
+
+
+
+
+
+
+function getSum(num1, num2){
+    return num1 + num2;
+}
+document.write('number of args in getSum: ' + getSum.length + '<br/>');
+Person.prototype.sound = 'Grrrrrr';
+Person.prototype.makeSound = function(){
+    return this.name + ' says ' + this.sound + '<br/>';
+}
+document.write( bobSmith.makeSound() + '<br/>');
+// bracket notation needed in loop as prop variable must be evaluted and then run thru .toString() (prop is itself not the name of any properties)
+for (var prop in bobSmith){
+    // console.log(prop + ': ' + bobSmith[prop] + '<br/>');
+}
+console.log(bobSmith.hasOwnProperty("enumerateMe")); // false, prototype has it
